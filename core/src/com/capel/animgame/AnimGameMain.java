5fe9e27b9a63ceabd53bba934b5f99226d0653fc
JCapelDev <win200003@gmail.com>
2021-01-24 23:45:35 +0100

    Left Move implemented

--- core/src/com/capel/animgame/AnimGameMain.java
+++ core/src/com/capel/animgame/AnimGameMain.java
@@ -18,9 +18,13 @@ public class AnimGameMain implements Screen {
     private static final int FRAME_COLS = 7, FRAME_ROWS = 1;
 
     // Objects used
-    Animation<TextureRegion> walkAnimation; // Must declare frame type (TextureRegion)
+    Animation<TextureRegion> walkAnimationRight;
+    Animation<TextureRegion> walkAnimationLeft;// Must declare frame type (TextureRegion)
     Texture walkSheet;
+    Texture walkSheetLeft;
     public Animation<TextureRegion> runningAnimation;
+
+    boolean facing;
 
     // A variable for tracking elapsed time for the animation
     float stateTime;
@@ -35,16 +39,35 @@ public class AnimGameMain implements Screen {
 
         // Place the regions into a 1D array in the correct order, starting from the top
         // left, going across first. The Animation constructor requires a 1D array.
-        TextureRegion[] walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+        TextureRegion[] walkFramesRight = new TextureRegion[FRAME_COLS * FRAME_ROWS];
         int index = 0;
         for (int i = 0; i < FRAME_ROWS; i++) {
             for (int j = 0; j < FRAME_COLS; j++) {
-                walkFrames[index++] = tmp[i][j];
+                walkFramesRight[index++] = tmp[i][j];
             }
         }
 
         // Initialize the Animation with the frame interval and array of frames
-        walkAnimation = new Animation<TextureRegion>(0.150f, walkFrames);
+        walkAnimationRight = new Animation<TextureRegion>(0.150f, walkFramesRight);
+
+        walkSheetLeft = new Texture(Gdx.files.internal("TestKeletonLeft.png"));
+
+
+        TextureRegion[][] tmpleft = TextureRegion.split(walkSheetLeft, walkSheetLeft.getWidth() / FRAME_COLS, walkSheetLeft.getHeight() / FRAME_ROWS);
+
+        // Place the regions into a 1D array in the correct order, starting from the top
+        // left, going across first. The Animation constructor requires a 1D array.
+        TextureRegion[] walkFramesLeft = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+        int indexleft = 0;
+        for (int i = 0; i < FRAME_ROWS; i++) {
+            for (int j = 0; j < FRAME_COLS; j++) {
+                walkFramesLeft[indexleft++] = tmpleft[i][j];
+            }
+        }
+
+        // Initialize the Animation with the frame interval and array of frames
+        walkAnimationRight = new Animation<TextureRegion>(0.050f, walkFramesRight);
+        walkAnimationLeft = new Animation<TextureRegion>(0.050f, walkFramesLeft);
 
         skeleton = new Rectangle();
         skeleton.x = 800 / 2 - 64 / 2;
@@ -55,6 +78,7 @@ public class AnimGameMain implements Screen {
         // time to 0
 
         stateTime = 0f;
+        facing=true;
     }
 
     @Override
@@ -68,18 +92,43 @@ public class AnimGameMain implements Screen {
         stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
 
         // Get current frame of animation for the current stateTime
-        TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
+        TextureRegion currentFrame = walkAnimationRight.getKeyFrame(stateTime, true);
         game.spriteBatch.begin();
         //game.spriteBatch.draw(currentFrame, 380, 50); // Draw current frame at (50, 50)
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            game.spriteBatch.draw(walkAnimation.getKeyFrame(stateTime, true), skeleton.x, skeleton.y);
+            game.spriteBatch.draw(walkAnimationLeft.getKeyFrame(stateTime,true), skeleton.x, skeleton.y);
             skeleton.x -= 100 * Gdx.graphics.getDeltaTime();
+            facing=false;
         } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            game.spriteBatch.draw(walkAnimation.getKeyFrame(stateTime, true), skeleton.x, skeleton.y);
+            game.spriteBatch.draw(walkAnimationRight.getKeyFrame(stateTime, true), skeleton.x, skeleton.y);
             skeleton.x += 120 * Gdx.graphics.getDeltaTime();
+            facing=true;
 
+        }else if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            if(facing){
+                game.spriteBatch.draw(walkAnimationRight.getKeyFrame(stateTime, true), skeleton.x, skeleton.y);
+                skeleton.y += 120 * Gdx.graphics.getDeltaTime();
+            }else{
+                game.spriteBatch.draw(walkAnimationLeft.getKeyFrame(stateTime, true), skeleton.x, skeleton.y);
+                skeleton.y += 120 * Gdx.graphics.getDeltaTime();
+            }
+
+
+        }else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            if(facing){
+                game.spriteBatch.draw(walkAnimationRight.getKeyFrame(stateTime, true), skeleton.x, skeleton.y);
+                skeleton.y -= 100 * Gdx.graphics.getDeltaTime();
+            }else{
+                game.spriteBatch.draw(walkAnimationLeft.getKeyFrame(stateTime, true), skeleton.x, skeleton.y);
+                skeleton.y -= 100 * Gdx.graphics.getDeltaTime();
+            }
         }else{
-            game.spriteBatch.draw(walkAnimation.getKeyFrames()[1], skeleton.x, skeleton.y,skeleton.width,skeleton.height);
+            if(facing){
+                game.spriteBatch.draw(walkAnimationRight.getKeyFrames()[1], skeleton.x, skeleton.y,skeleton.width,skeleton.height);
+            }else {
+                game.spriteBatch.draw(walkAnimationLeft.getKeyFrames()[1], skeleton.x, skeleton.y,skeleton.width,skeleton.height);
+            }
+
         }
         game.spriteBatch.end();
     }
